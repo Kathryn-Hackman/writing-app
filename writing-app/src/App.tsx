ed25a82bfbd80b3d45736b34bee4ec4bf4c89716
Kathryn Hackman <kathrynhackman@Kathryns-MacBook-Pro-2.local>
2025-07-31 17:17:53 -0400

    set up boilerplate tiptap editor

---
+++ writing-app/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import { SimpleEditor } from './components/tiptap-templates/simple/simple-editor'
+
+function App() {
+
+  return (
+    <>
+      <SimpleEditor></SimpleEditor>
+    </>
+  )
+}
+
+export default App
