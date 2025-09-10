8e73fa7b7cc54e9cc53e985c1246f6b77432961a
Michael Hendrick <mhendric@bu.edu>
2025-09-09 21:29:03 -0400

    insane db updates tbh

--- writing-app/server/main.py
+++ writing-app/server/main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI, Depends
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import FileResponse
 from sqlmodel import Field, SQLModel, create_engine, Session, Relationship
 from typing import Annotated, Union, List, Optional
 import random
@@ -18,7 +20,8 @@ class Author(SQLModel, table=True):
 class Group(SQLModel, table=True):
     id: Union[int, None] = Field(default=None, primary_key=True)
     name: str
-    admin: "Author" = Relationship(back_populates="admin_groups")
+    admin_id: Optional[int] = Field(default=None, foreign_key="author.id")
+    admin: Optional["Author"] = Relationship(back_populates="admin_groups")
     authors: List["Author"] = Relationship(back_populates="groups")
     stories: List["Story"] = Relationship(back_populates="group")
 
@@ -27,16 +30,19 @@ class Post(SQLModel, table=True):
     id: Union[int, None] = Field(default=None, primary_key=True)
     text: str
     date_unixtime: int
-    story: "Story" = Relationship(back_populates="posts")
-    author: "Author" = Relationship(back_populates="posts")
+    story_id: Optional[int] = Field(default=None, foreign_key="story.id")
+    story: Optional["Story"] = Relationship(back_populates="posts")
+    author_id: Optional[int] = Field(default=None, foreign_key="author.id")
+    author: Optional["Author"] = Relationship(back_populates="posts")
 
 
 class Story(SQLModel, table=True):
     id: Union[int, None] = Field(default=None, primary_key=True)
     title: str
     isDone: bool = Field(default=False)
+    group_id: Optional[int] = Field(default=None, foreign_key="group.id")
     posts: List["Post"] = Relationship(back_populates="story")
-    group: "Group" = Relationship(back_populates="stories")
+    group: Optional["Group"] = Relationship(back_populates="stories")
 
 
 sqlite_file_name = "database.db"
@@ -58,6 +64,9 @@ def get_session():
 SessionDep = Annotated[Session, Depends(get_session)]
 
 app = FastAPI()
+
+# Mount static files
+app.mount("/static", StaticFiles(directory="."), name="static")
 
 
 @app.on_event("startup")
@@ -83,7 +92,17 @@ async def root():
     return {"message": "Hello World"}
 
 
-@app.post("/stories/")
+@app.get("/test")
+async def test_page():
+    return FileResponse("simple_test.html")
+
+
+@app.get("/forms")
+async def forms_page():
+    return FileResponse("test_forms.html")
+
+
+@app.post("/create_story/")
 async def create_story(story: Story, session: SessionDep):
     story.id = random.randint(1000, 999999)
     session.add(story)
@@ -93,6 +112,70 @@ async def create_story(story: Story, session: SessionDep):
 
 
 @app.get("/get_story/{story_id}")
-async def create_story(story_id: int, session: SessionDep):
+async def get_story(story_id: int, session: SessionDep):
     story = session.get(Story, story_id)
     return story
+
+
+# Get all data endpoints
+@app.get("/authors/")
+async def get_all_authors(session: SessionDep):
+    from sqlmodel import select
+
+    statement = select(Author)
+    authors = session.exec(statement).all()
+    return authors
+
+
+@app.get("/groups/")
+async def get_all_groups(session: SessionDep):
+    from sqlmodel import select
+
+    statement = select(Group)
+    groups = session.exec(statement).all()
+    return groups
+
+
+@app.get("/stories/")
+async def get_all_stories(session: SessionDep):
+    from sqlmodel import select
+
+    statement = select(Story)
+    stories = session.exec(statement).all()
+    return stories
+
+
+@app.get("/posts/")
+async def get_all_posts(session: SessionDep):
+    from sqlmodel import select
+
+    statement = select(Post)
+    posts = session.exec(statement).all()
+    return posts
+
+
+@app.post("/create_author/")
+async def create_author(author: Author, session: SessionDep):
+    author.id = random.randint(1000, 999999)
+    session.add(author)
+    session.commit()
+    session.refresh(author)
+    return author
+
+
+@app.post("/create_post/")
+async def create_post(post: Post, session: SessionDep):
+    post.id = random.randint(1000, 999999)
+    session.add(post)
+    session.commit()
+    session.refresh(post)
+    return post
+
+
+@app.post("/create_group/")
+async def create_group(group: Group, session: SessionDep):
+    group.id = random.randint(1000, 999999)
+    session.add(group)
+    session.commit()
+    session.refresh(group)
+    return group
